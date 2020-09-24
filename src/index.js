1c3ffa70d1149e532148c765edd916062a57b6aa
Bruno Pinto <bruno@bpinto.com>
2020-09-24 13:59:11 +0100

    Allow plugin to be disabled

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 class JestWatchMasterPlugin {
   constructor({ config } = {}) {
     this.branch = 'master';
+    this.enabled = false;
+
     if (config && config.branch) {
       this.branch = config.branch;
     }
@@ -9,12 +11,14 @@ class JestWatchMasterPlugin {
   getUsageInfo() {
     return {
       key: 'm',
-      prompt: `test changes since ${this.branch}`,
+      prompt: `test changes since ${this.enabled ? 'last commit' : this.branch}`,
     };
   }
 
   run(globalConfig, updateConfigAndRun) {
-    updateConfigAndRun({ mode: 'watch', changedSince: this.branch });
+    this.enabled = !this.enabled;
+
+    updateConfigAndRun({ mode: 'watch', changedSince: (this.enabled ? this.branch : null)});
     return Promise.resolve();
   }
 }
